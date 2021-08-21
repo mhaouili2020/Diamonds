b6c06a4c22a6718577e4c43875de413f8703d626
mhaouili2020 <69821081+mhaouili2020@users.noreply.github.com>
2021-08-21 21:45:47 +0200

    Step1

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from pandas.plotting import scatter_matrix
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def load_diamonds_data(diamonds_path):
+   csv_path = os.path.join(diamonds_path, "diamonds.csv")
+   return pd.read_csv(csv_path)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    diamond = load_diamonds_data('')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    carat_ix, cut_ix, color_ix, clarity_ix, depth_ix, table_ix, price_ix, x_ix, y_ix, z_ix, =[
+        list(diamond.columns).index(col)
+        for col in ("carat", "cut", "color", "clarity", "depth", "table", "price", "x", "y", "z")]
+
+    
